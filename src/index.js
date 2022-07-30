83ceacb8e46048488a0bcdc570c51ba63b0b08fe
Timothy Rogers <github@hack13.me>
2022-07-30 10:08:46 -0400

    Switch to pulling straight from GitHub

--- src/index.js
+++ src/index.js
@@ -7,29 +7,53 @@ const headers = {
   'Content-type': 'application/json',
 };
 
+// Check if we got JSON response
+function isJsonObject(strData) {
+  try {
+    JSON.parse(strData)
+  } catch (e) {
+    return false
+  }
+  return true
+}
+
 // GET default data
 router.get('/', request => new Response('{"error": "Missing request params"}', {headers}))
 
 // GET collection index
 router.get('/games', async request => {
-  const games = await BOTC_DATA.get('games_metadata', {type: "json"})
-  return new Response(JSON.stringify(games),{headers})
+  //const games = await BOTC_DATA.get('games_metadata', {type: "json"})
+  //return new Response(JSON.stringify(games),{headers})
+  const init = {
+    headers: {
+      'content-type': 'application/json;charset=utf-8',
+    }
+  }
+  const metaData = await fetch('https://raw.githubusercontent.com/hack13/neos-boct-scripts/main/games.json', init)
+  return new Response(JSON.stringify(await metaData.json()), {headers})
 })
 
 // GET item
 router.get('/game/:id', async ({ params }) => {
-  const game = await BOTC_DATA.get(params.id, {type:"json", cacheTtl: 60})
-  return new Response(JSON.stringify(game),{headers})
+  //const game = await BOTC_DATA.get(params.id, {type:"json", cacheTtl: 60})
+  //return new Response(JSON.stringify(game),{headers})
+  let url = 'https://raw.githubusercontent.com/hack13/neos-boct-scripts/main/games/'+params.id+'.json'
+  const init = {
+    headers: {
+      'content-type': 'application/json;charset=utf-8',
+    }
+  }
+  const metaData = await fetch(url, init)
+  let gatheredData = await metaData.json().catch(err => {console.error(err)});
+
+  if (gatheredData != null) {
+    return new Response(JSON.stringify(gatheredData),{headers})
+  } else {
+    let missingGame = {'error' : 'Game Not Found'}
+    return new Response(JSON.stringify(missingGame),{headers})
+  }
+  
 })
-
-// POST to the collection (we'll use async here)
-/*router.post('/creategame', async request => {
-  const content = await request.json()
-  const slug = JSON.stringify(content.date)
-  BOTC_DATA.put(slug.replace(/"/g, ""), JSON.stringify(content))
-
-  return new Response('Created Game: ' + JSON.stringify(content))
-})*/
 
 // 404 for everything else
 router.all('*', () => new Response('Not Found.', { status: 404 }))
